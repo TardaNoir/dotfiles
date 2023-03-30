a37c26c0263cfe92b22c696e61ecbba054297314
Rooster <tardanoir@gmail.com>
2023-03-30 17:12:27 -0300

    first commit

---
+++ nvim/lua/core/set.lua
@@ -0,0 +1,33 @@
+
+vim.o.hlsearch = false
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.wo.signcolumn = 'yes'
+
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.o.termguicolors = true
+vim.cmd'colorscheme kanagawa'
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.cmd'set noshowcmd'
+vim.cmd'set noshowmode'
+vim.cmd'set cmdheight=0'
